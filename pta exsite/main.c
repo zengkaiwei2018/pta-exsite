4f4eec83500392c05084e757cf3d3682b54b33a2
zengkaiwei2018 <1912207369@qq,com>
2019-11-19 17:24:50 +0800

    Initial commit

---
+++ pta exsite/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  pta exsite
+//
+//  Created by s20181105306 on 2019/11/19.
+//  Copyright © 2019 s20181105306. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
